9e8f31aec0fd47c9bd49562b8286309cd69de142
Bruno Brandelli <bdbrandelli@hotmail.com>
2019-08-15 12:53:03 -0300

    Adição de arquivos do site iberLEF

---
+++ src/file_parser.py
@@ -0,0 +1,7 @@
+def parse_file_with_ner(file_name):
+    print('parse_file_with_ner')
+    print(file_name)
+
+def parse_file_without_ner():
+    print('parse_file_without_ner')
+    print(file_name)
