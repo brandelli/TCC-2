e6430c07208269ca3850e72fd71195cbebe6dc15
Bruno Brandelli <bdbrandelli@hotmail.com>
2019-08-23 11:16:31 -0300

    Alterando nome das pastas

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
-from parser import file_parser
+from modules import parser
 def main():
     print('Rodando main')
-    input_dict = file_parser.parse_iberlef_input('data/treino/Corpus_RE_Treino.tsv')
-    file_parser.parse_dict_input_into_relation_id_mapping(input_dict)
+    input_dict = parser.parse_iberlef_input('data/treino/Corpus_RE_Treino.tsv')
+    parser.parse_dict_input_into_relation_id_mapping(input_dict)
 
 if __name__ == '__main__':
     main()
