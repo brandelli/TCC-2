f31c8a55b495749fe8c9251983ac53fbf9dab1da
Bruno Brandelli <bdbrandelli@hotmail.com>
2019-08-26 11:36:48 -0300

    Metodo para converter word embeddings para json

--- src/app.py
+++ src/app.py
@@ -2,7 +2,11 @@ from helpers import file_helper
 def main():
     print('Rodando main')
     # treino, teste_1, teste_2
-    input_dict = file_helper.tsv_to_json('data/dataset/','treino')
+    dataset_path = 'data/dataset/'
+    file_helper.tsv_to_json(dataset_path,'treino')
+    file_helper.tsv_to_json(dataset_path, 'teste_1')
+    file_helper.word_embeddings_to_json('data/word_embeddings/exemplo/', 'word_embeddings')
+
 
 if __name__ == '__main__':
     main()
--- src/helpers/file_helper.py
+++ src/helpers/file_helper.py
@@ -26,8 +26,37 @@ def tsv_to_json(path, file_name, extension='.tsv'):
                 input_dict[keys_dict.get(index)].append(value)
         
     dict_to_json(path, file_name, input_dict, 4)
+
+
+def word_embeddings_to_json(path, file_name, extension='.txt'):
+    '''
+    Função para transformar o arquivo de word_embeddings em json
+    '''
+    print(f"Rodando: {word_embeddings_to_json.__name__}")
+    # lista de dicionarios com dados processados de word embeddings
+    word_embeddings_list = []
+    with open(f"{path}{file_name}{extension}") as fp:
+        # primeira linha do arquivo contém o número de linhas e a dimensionalidade do vetor
+        lines, vector_size = fp.readline().strip().split(' ')
+        lines = int(lines)
+        print(f'Número de linhas: {lines}, tamanho do vetor: {vector_size}')
+        # itera por todas linhas que contém dados do word embeddings
+        for x in range(0 ,lines):
+            current_word_dict = {}
+            # separa os dados presentes em cada linha, e realiza o pop para separar a word do vetor
+            data_list = fp.readline().strip().split(' ')
+            word = data_list.pop(0)
+            current_word_dict['word'] = word
+            # transforma os dados do vetor em float
+            current_word_dict['vec'] = list(map(lambda x: float(x), data_list))
+            word_embeddings_list.append(current_word_dict)
+            
+    dict_to_json(path, file_name, word_embeddings_list, 4)
     
 
-def dict_to_json(path, file_name, my_dict, indent=0):
+def dict_to_json(path, file_name, my_data, indent=0):
+    '''
+    Função para criar arquivo json com base em dados processados anteriormente
+    '''
     with open(f'{path}{file_name}.json', 'w') as json_file:
-        json.dump(my_dict, json_file, indent=indent)
+        json.dump(my_data, json_file, indent=indent)
