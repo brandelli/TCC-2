9be773131f9d303e21a29544cf20a72dc80c435f
Bruno Brandelli <bdbrandelli@hotmail.com>
2019-09-25 10:16:22 -0300

    Adiçao de arquivo de teste de modelos

--- src/modules/model.py
+++ src/modules/model.py
@@ -8,9 +8,12 @@ class Model:
 
     def create_model(self):
         self.model = tf.keras.Sequential([
-            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64))
+            # tf.keras.layers.Embedding(tokenizer.vocab_size, 64),
+            tf.keras.layers.Embedding(150, 64),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid')
         ])
-        print('Criando modelo')
 
     def train_model(self):
         print('treinando modelo')
