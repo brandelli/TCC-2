ca8d689d2483ba4f835383b47fa761128f5a2e58
Bruno Brandelli <bdbrandelli@hotmail.com>
2019-10-11 11:41:00 -0300

    Verificaçao de outputs criados, onde e possivel perceber erro no desenvolvimento

--- src/app.py
+++ src/app.py
@@ -20,8 +20,8 @@ def main():
 
     config = Config('data/configuration/', 'config.json')
     run_data_parse(config)
-    #run_data_visualization(config)
-    run_model(config)
+    run_data_visualization(config)
+    #run_model(config)
 
 
 def run_data_parse(config):
@@ -37,9 +37,14 @@ def run_data_visualization(config):
     dataset_path = dataset_config.get('path')
     dataset_train = file_helper.get_json_file_data(dataset_path, dataset_config.get('train_json'))
     dataset_test = file_helper.get_json_file_data(dataset_path, dataset_config.get('test_json'))
-    visualization.get_relation_data(dataset_train)
-    visualization.get_tuple_relation_data(dataset_train)
-    print(visualization.get_entities_data(dataset_train))
+    output_config = config.get_configuration('output')
+    output_path = output_config.get('path')
+    output_train = file_helper.get_json_file_data(output_path, output_config.get('train_sentence_output'))
+    output_test = file_helper.get_json_file_data(output_path, output_config.get('test_sentence_output'))
+    print('=================visualizando output de treino================================')
+    visualization.print_predicted_relation(dataset_train, output_train)
+    print('=================visualizando output de teste=================================')
+    visualization.print_predicted_relation(dataset_test, output_test)
 
 
 def run_model(config):
