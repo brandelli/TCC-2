9296723f57895c48f547dd5e9d8bd67b0f534dee
Bruno Brandelli <bdbrandelli@hotmail.com>
2019-10-16 15:32:32 -0300

    Criaçao de helper para metricas do modelo

---
+++ src/helpers/metrics_helper.py
@@ -0,0 +1,11 @@
+def get_correct_relations(data):
+    print('get_correct_relations')
+    return 0
+
+def get_number_of_relations_predicted(data):
+    print('get_number_of_relations_predicted')
+    return 0
+
+def get_number_of_relations_in_dataset(data):
+    print('get_number_of_relations_in_dataset')
+    return len(data)
